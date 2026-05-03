2a028207a098d0890aaa4d547f5eda333ab16daf
ruizRojasFel <felruiz.a@gmail.com>
2026-05-03 04:09:10 -0400

    chore(init): Initialize project

---
+++ src/main/java/cl/felruiz/apigeocl/ApiGeoClApplication.java
@@ -0,0 +1,13 @@
+package cl.felruiz.apigeocl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeoClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeoClApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/felruiz/apigeocl/ApiGeoClApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.felruiz.apigeocl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeoClApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
